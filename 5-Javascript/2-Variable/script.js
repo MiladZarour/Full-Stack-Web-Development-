8c66a8bc305b91ae4ebe257c63dddb22990c4958
Milad Zarour <Miladzarour@hotmail.com>
2022-06-05 19:08:59 +0200

    Variable

--- 5-Javascript/2-Variable/script.js
+++ 5-Javascript/2-Variable/script.js
@@ -1,1 +1,9 @@
 console.log('Hello from seperated file');
+
+var name = 'Alex';
+console.log(name);
+
+var schoolTeam = 'basketball';
+var schoolNum = 20;
+console.log(schoolTeam);
+console.log(schoolNum);
