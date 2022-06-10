3f3f1a1da3f0784fb876f971f9372504502f2b5f
Milad Zarour <50852900+MiladZarour@users.noreply.github.com>
2022-06-10 15:18:02 +0200

    ObjectMethod

--- 5-Javascript/21-ObjectMethod/script.js
+++ 5-Javascript/21-ObjectMethod/script.js
@@ -1,26 +1,39 @@
-var universities = [
-    {
-        name: " Aleppo ",
-        hasGone: false,
-        year: " 2010 "
-    },
-    {
-        name: " MIT ",
-        hasGone: true,
-        year: " 205 "
-    },
-    {
-        name: " OXFORD ",
-        hasGone: true,
-        year: " 1800 "
-    },
-];
+// OBJECT METHODS
 
-universities.forEach(function (university) {
-    if (university.hasGone === true) {
-        console.log(university.name + university.year);
+var Sheldon = {
+    name: 'Sheldon',
+    age: 35,
+    hasGone: true,
+    friends: ['Penny', 'Raj'],
+    say: function () {
+        console.log('say hi Sheldon');
     }
-    else {
-        console.log(university.name);
+}
+
+// say(); // it is not define outside from the object Sheldon
+Sheldon.say();
+
+var Leonard = {
+    name: 'Leonard',
+    say: function () {
+        console.log('Say Hi Leonard');
     }
-});
+}
+
+Leonard.say();
+
+function say() {
+    console.log('Say Hi Outside');
+}
+say();
+
+function say() {
+    console.log('say hi 23222');
+}
+say();
+console.log('----------------------');
+
+customElements.deletePost
+customElements.deleteUser
+user.delete
+postMessage.delete
