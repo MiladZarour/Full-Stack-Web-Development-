351fa14d18cffea57e8189903dfccd348b5844e2
Milad Zarour <Miladzarour@hotmail.com>
2022-06-18 21:01:36 +0200

    CallBackFunction + prepared for DifferentEventTypes

---
+++ 5-Javascript/33-DifferentEventTypes/script.js
@@ -0,0 +1,39 @@
+// var button = document.querySelector('#btn1');
+// var title = document.querySelector('#OAKtitle')
+
+// this will happened when I cleck the button
+// button.addEventListener('click', function () {
+//     console.log('You Clicked The Button');
+
+//     title.style.backgroundColor = 'red'
+// });
+
+// var listParent = document.querySelector('ul');
+
+// listParent.addEventListener('click', function () {
+//     console.log('You clicked the ul');
+
+//     listParent.style.backgroundColor = 'Blue';
+// })
+
+// Quiz Solution //
+
+// var listItems = document.querySelectorAll('li');
+// console.log('listItems', listItems);
+
+// for (var i = 0; i < listItems.length; i++) {
+//     listItems[i].addEventListener('click', function () {
+//         this.style.color = 'white';
+//         listItems[i].style.color = 'white';
+//     })
+// }
+
+var oakParagraph = document.querySelector('#oakParagraph');
+
+function toggleBackground() {
+    // oakParagraph.classList.toggle('oakParagraph');
+    oakParagraph.classList.toggle('toggleBackground');
+}
+
+oakParagraph.addEventListener('click', toggleBackground); //callback function is without ()
+// oakParagraph.addEventListener('click', toggleBackground());
